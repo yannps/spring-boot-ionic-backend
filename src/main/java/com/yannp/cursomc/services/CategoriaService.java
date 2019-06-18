54922c4115b1ed3f23af508699a2d48b643019a2
Yann Pereira <yann.pereira@dce.ufpb.br>
2019-06-18 18:38:30 -0300

    Ajustes no endpoint /categorias{id}

--- src/main/java/com/yannp/cursomc/services/CategoriaService.java
+++ src/main/java/com/yannp/cursomc/services/CategoriaService.java
@@ -1,12 +1,14 @@
 package com.yannp.cursomc.services;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yannp.cursomc.domain.Categoria;
 import com.yannp.cursomc.repositories.CategoriaRepository;
+import com.yannp.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +16,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 	
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
