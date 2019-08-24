5f30615b8f85dea9d75ab87a7473744cfe12ba40
Yann Pereira <yann.pereira@dce.ufpb.br>
2019-08-24 16:58:51 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/yannp/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/yannp/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.yannp.cursomc.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.yannp.cursomc.domain.Categoria;
 import com.yannp.cursomc.services.CategoriaService;
@@ -25,5 +28,12 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 }
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 }
--- src/main/java/com/yannp/cursomc/services/CategoriaService.java
+++ src/main/java/com/yannp/cursomc/services/CategoriaService.java
@@ -1,8 +1,6 @@
 package com.yannp.cursomc.services;
 
 import java.util.Optional;
-import java.util.function.Supplier;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +19,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj){
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
